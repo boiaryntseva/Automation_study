bfb4d4c951ec9f23e85335f6126de059d430e5fd
boiaryntseva <boiaryntseva@gmail.com>
2020-12-07 18:54:23 -0500

    Downgraded TestNg version in pom.xml to fix Certificate issues, created testng.xml, succesfuly ran test to invoke browser using mvn command and through test ng

--- src/test/java/InvokingBrowserTest.java
+++ src/test/java/InvokingBrowserTest.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class InvokingBrowser {
+public class InvokingBrowserTest {
 
-    public static void main(String[] args) {
+    @Test
+    public static void InvokingBrowser() {
         System.setProperty("webdriver.chrome.driver", "/Users/boiaryntseva/Desktop/Libraries/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
